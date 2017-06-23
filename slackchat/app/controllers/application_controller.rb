cab93466876f18a0e9d91cf53901742312b18021
Rodrigo Barreto <rodrigo.barreto@mall-connect.com>
2017-06-23 10:27:59 +0200

    set up ability

--- slackchat/app/controllers/application_controller.rb
+++ slackchat/app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include CanCan::ControllerAdditions
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+    end
+  end
 
 end
